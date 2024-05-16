d6c7ea9879f3efcbde84b8713fe973c9c740485b
dnordACT <dnord@advancect.org>
2024-05-16 14:44:58 -0400

    new branch to test adding data to firebase

--- app/database_test.py
+++
@@ -1,9 +0,0 @@
-from src.database_mgr import DatabaseManager  # Replace 'your_module' with the actual name of the module where DatabaseManager is defined.
-
-database = DatabaseManager(companies_csv_path="data/company_list.csv")
-
-def test_get_company_info():
-    company_table = database.get_company_info()
-    return company_table
-
-print(test_get_company_info())
